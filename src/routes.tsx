d75caab90b0ec838785be13064866800643762c1
charukirti <charukirticc112002@gmail.com>
2025-02-13 11:41:57 +0530

    feat: implement routing with react-router

---
+++ src/routes.tsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouteObject } from "react-router";
+import AppLayout from "./components/Layout/AppLayout";
+import Home from "./pages/Home";
+import CreateTrip from "./pages/CreateTrip";
+import TripDetails from "./pages/TripDetails";
+import MyTrips from "./pages/MyTrips";
+import PageNotFound from "./pages/PageNotFound";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+
+      {
+        path: "/create-trip",
+        element: <CreateTrip />,
+      },
+
+      {
+        path: "trip-deatils/:id",
+        element: <TripDetails />,
+      },
+
+      {
+        path: "my-trips",
+        element: <MyTrips />,
+      },
+
+      {
+        path: "*",
+        element: <PageNotFound />,
+      },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
