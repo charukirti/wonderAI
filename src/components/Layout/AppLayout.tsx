5e36c72b4701cca506adc53e529bb2452bc84d92
charukirti <charukirticc112002@gmail.com>
2025-02-16 00:25:52 +0530

    feat: add Footer component to AppLayout

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function AppLayout() {
   return (
-    <div className="mx-3 lg:mx-10">
+    <div className="mx-3 min-h-screen lg:mx-10">
       <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
