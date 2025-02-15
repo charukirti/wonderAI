799e403cdc2407f053efc938774edb9a23f1f53c
charukirti <charukirticc112002@gmail.com>
2025-02-16 00:27:14 +0530

    feat: add FormData interface for trip creation

---
+++ src/features/create-trip/type.ts
@@ -0,0 +1,6 @@
+export interface FormData {
+    destination: string;
+    duration: number;
+    budget: 'Cheap' | 'Moderate' | 'Luxury';
+    withWhom: 'Just Me' | 'A couple' | 'Family' | 'Friends';
+  }
