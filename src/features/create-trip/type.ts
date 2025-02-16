dfa138e5aa925b2e29d75e0292e16533e756a6d7
charukirti <charukirticc112002@gmail.com>
2025-02-16 19:27:27 +0530

    feat: update FormData interface to include activity type and enhance budget and group options

--- src/features/create-trip/type.ts
+++ src/features/create-trip/type.ts
@@ -1,6 +1,13 @@
+import { activityOptions, budgetOptions, groupOptions } from "../../utils/constants";
+
+export type BudgetType = typeof budgetOptions[number]['title'];
+export type GroupType = typeof groupOptions[number]['title'];
+export type ActivityType = typeof activityOptions[number]['title'];
+
 export interface FormData {
-    destination: string;
-    duration: number;
-    budget: 'Cheap' | 'Moderate' | 'Luxury';
-    withWhom: 'Just Me' | 'A couple' | 'Family' | 'Friends';
-  }
+  destination: string;
+  duration: string | number;
+  budget: BudgetType;
+  withWhom: GroupType;
+  activity: ActivityType;
+}
