b86b98917261f5644ec2730b799a1f7be5a4e8d7
charukirti <charukirticc112002@gmail.com>
2025-02-18 22:51:25 +0530

    feat: integrate React Hot Toast for  user notifications

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster/>
       <App />
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
